05fae127dd89e1d359e34d5899f83a8d0c4f9f81
Shayennn <me@shayennn.com>
2020-04-21 01:32:21 +0700

    Monitor changed grade

--- autocheck_group.py
+++ autocheck_group.py
@@ -54,24 +54,44 @@ print('=========================')
 ret, data = obj.getGrade()
 if ret:
     for code, sub_data in data.items():
-        if sub_data['grade'] == 'N':
-            continue
         print('['+code+']', sub_data['name'],
               'Credit:', sub_data['credit'])
         print('\tGrade:', sub_data['grade'])
+        if sub_data['grade'] == 'N':
+            continue
         if code not in old_data and sub_data['grade'] != 'N':
+            print('\t', end='')
             if code in announce_list:
                 msg = [sub_data['name']+' อัพโหลดเกรดขึ้นระบบแล้ว', '']
                 msg += ['สามารถดูได้ที่ https://goo.gl/kUBHfa',
                         'หรือผ่านแอพ NisitKU']
                 r = requests.post(line_url, headers=group_line_headers, data={
                     'message': '\n'.join(msg)})
+                print("Announced & ", end='')
             msg = ['['+code+'] '+sub_data['name'] +
                    ' Credit: '+sub_data['credit']]
             msg += ['ได้บันทึกเกรดลงระบบแล้ว']
             msg += ['Grade: '+sub_data['grade']]
             r = requests.post(line_url, headers=line_headers, data={
                 'message': '\n'.join(msg)})
+            print("Notified")
+        if code in old_data and old_data[code]['grade'] != sub_data['grade']:
+            print('\t', end='')
+            if code in announce_list:
+                msg = [sub_data['name']+' แก้ไขเกรดในระบบแล้ว', '']
+                msg += ['สามารถดูได้ที่ https://goo.gl/kUBHfa',
+                        'หรือผ่านแอพ NisitKU']
+                r = requests.post(line_url, headers=group_line_headers, data={
+                    'message': '\n'.join(msg)})
+                print("Announced & ", end='')
+            msg = ['['+code+'] '+sub_data['name'] +
+                   ' Credit: '+sub_data['credit']]
+            msg += ['ได้แก้ไขเกรดในระบบแล้ว']
+            msg += ['From: '+old_data[code]['grade']]
+            msg += ['Grade: '+sub_data['grade']]
+            r = requests.post(line_url, headers=line_headers, data={
+                'message': '\n'.join(msg)})
+            print("Notified")
     with open('nisitku_data.pkl', mode='wb') as output:
         pickle.dump(data, output)
         output.close()
--- longrun.py
+++ longrun.py
@@ -52,35 +52,52 @@ while True:
     ret, data = obj.getGrade()
     if ret:
         for code, sub_data in data.items():
-            if sub_data['grade'] == 'N':
-                continue
             print('['+code+']', sub_data['name'],
                   'Credit:', sub_data['credit'])
             print('\tGrade:', sub_data['grade'])
-            print('\tStatus:', sub_data['status'])
-            if (code in old_data and old_data[code]['grade'] == 'N') or code not in old_data:
+            if sub_data['grade'] == 'N':
+                continue
+            if code not in old_data and sub_data['grade'] != 'N':
+                print('\t', end='')
                 if code in announce_list:
                     msg = [sub_data['name']+' อัพโหลดเกรดขึ้นระบบแล้ว', '']
                     msg += ['สามารถดูได้ที่ https://goo.gl/kUBHfa',
                             'หรือผ่านแอพ NisitKU']
-                    r = requests.post(line_url, headers=group_line_headers, data={
-                        'message': '\n'.join(msg)})
+                    r = requests.post(line_url, headers=group_line_headers,
+                                      data={
+                                          'message': '\n'.join(msg)
+                                      })
+                    print("Announced & ", end='')
                 msg = ['['+code+'] '+sub_data['name'] +
                        ' Credit: '+sub_data['credit']]
                 msg += ['ได้บันทึกเกรดลงระบบแล้ว']
                 msg += ['Grade: '+sub_data['grade']]
-                msg += ['Status: '+sub_data['status']]
-                r = requests.post(line_url, headers=line_headers, data={
-                    'message': '\n'.join(msg)})
-            elif code in old_data and old_data[code]['status'] != sub_data['status']:
+                r = requests.post(line_url, headers=line_headers,
+                                  data={
+                                      'message': '\n'.join(msg)
+                                  })
+                print("Notified")
+            if code in old_data and old_data[code]['grade'] != sub_data['grade']:
+                print('\t', end='')
+                if code in announce_list:
+                    msg = [sub_data['name']+' แก้ไขเกรดในระบบแล้ว', '']
+                    msg += ['สามารถดูได้ที่ https://goo.gl/kUBHfa',
+                            'หรือผ่านแอพ NisitKU']
+                    r = requests.post(line_url, headers=group_line_headers,
+                                      data={
+                                          'message': '\n'.join(msg)
+                                      })
+                    print("Announced & ", end='')
                 msg = ['['+code+'] '+sub_data['name'] +
                        ' Credit: '+sub_data['credit']]
-                msg += ['ได้เปลี่ยนแปลงสถานะเกรด']
+                msg += ['ได้แก้ไขเกรดในระบบแล้ว']
+                msg += ['From: '+old_data[code]['grade']]
                 msg += ['Grade: '+sub_data['grade']]
-                msg += ['Status: '+old_data[code]
-                        ['status']+'=>'+sub_data['status']]
-                r = requests.post(line_url, headers=line_headers, data={
-                    'message': '\n'.join(msg)})
+                r = requests.post(line_url, headers=line_headers,
+                                  data={
+                                      'message': '\n'.join(msg)
+                                  })
+                print("Notified")
         with open('nisitku_data.pkl', mode='wb') as output:
             pickle.dump(data, output)
             output.close()
